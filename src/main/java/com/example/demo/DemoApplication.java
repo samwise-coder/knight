af6d2392e3e7dc0380da756c775fc04348ce130d
samwise <974049754@qq.com>
2021-08-15 23:37:10 +0800

    init project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@SpringBootApplication
+public class DemoApplication {
+    @RequestMapping("/index")
+public String index(){
+        return "this is frist springboot project!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
