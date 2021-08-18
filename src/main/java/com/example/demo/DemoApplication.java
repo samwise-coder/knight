75ae823e91108a05d01caa6b8b2d9364cd83ae64
samwise-coder <houguochao.outlook.com>
2021-08-19 00:56:22 +0800

    fix corsfilter bug

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class DemoApplication {
     @RequestMapping("/index")
